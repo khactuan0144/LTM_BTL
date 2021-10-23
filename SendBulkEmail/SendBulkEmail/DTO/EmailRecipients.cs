0a14944f9f98314e9a5e621fc75aa932c4db0298
Khac Tuan <ktuan439@gmail.com>
2021-10-23 14:09:00 +0700

    Create project

---
+++ SendBulkEmail/SendBulkEmail/DTO/EmailRecipients.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendBulkEmail.DTO
+{
+    class EmailRecipients
+    {
+        private string idEmailContent;
+        private string emailRecipient;
+        private string sender;
+
+        public string IdEmailContent { get => idEmailContent; set => idEmailContent = value; }
+        public string EmailRecipient { get => emailRecipient; set => emailRecipient = value; }
+        public string Sender { get => sender; set => sender = value; }
+    }
+}
---
+++ SendBulkEmail/SendBulkEmail/DTO/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendBulkEmail.DTO
+{
+    class User
+    {
+        private string email;
+        private string pass;
+        private string name;
+        private string birhday;
+        private string phone;
+
+        public string Email { get => email; set => email = value; }
+        public string Pass { get => pass; set => pass = value; }
+        public string Name { get => name; set => name = value; }
+        public string Birhday { get => birhday; set => birhday = value; }
+        public string Phone { get => phone; set => phone = value; }
+    }
+}
